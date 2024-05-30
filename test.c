47274bb4a68eeac06b72509be51e0c78c5a6434a
PhucPhucc <95041948+PhucPhucc@users.noreply.github.com>
2024-05-30 19:13:48 +0700

    Update test.c

--- test.c
+++ test.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 
-void getData(char *stt, char *name, char *mssv, char *major, char *birth) {
-    
+void getStudents(char stt[20][10], char mssv[20][10], char surname[20][20], char middlename[20][20], char givenname[20][20], char sex[20][4], char age[20][10], char math[20][20], char physics[20][20], char chemistry[20][20], int index) {
+    printf("************************Menu************************\n");
+    for(int i = 0; i < index; i++) {
+        printf("**%s\t", stt[i]);
+        printf("%s\t\t", mssv[i]);
+        printf("%s\t", surname[i]);
+        printf("%s\t", middlename[i]);
+        printf("%s\t", givenname[i]);
+        printf("%s\t", sex[i]);
+        printf("%s\t", age[i]);
+        printf("%s\t", math[i]);
+        printf("%s\t", physics[i]);
+        printf("%s\t", chemistry[i]);
+        printf("\n");
+    }
+    printf("***************************************************\n");
+
+}
+
+void add() {
+
+}
+
+void update() {
+
+}
+
+void removed() {
+
+}
+
+void search() {
+
+}
+
+void arrangeGPA() {
+
+}
+
+void arrangeName() {
+
 }
 
 int main(void)
@@ -11,59 +50,44 @@ int main(void)
     //Mở file bằng hàm fopen
     // fopen_s(&fp, "script.txt", "r+");
     fp = fopen("script.txt", "r");
-    char stt[5][50];
-    char name[5][50];
-    char mssv[5][50];
-    char major[5][50];
-    char birth[5][50];
-    int i = 0;
+    char stt[20][10];
+    char surname[20][20];
+    char middlename[20][20];
+    char givenname[20][20];
+    char mssv[20][10];
+    char sex[20][4];
+    char age[20][10];
+    char math[20][20];
+    char physics[20][20];
+    char chemistry[20][20];
+
+    int index = 0;
 
 
-    while(fscanf(fp, "%s %s %s %s %s", stt[i], name[i], mssv[i], major[i], birth[i]) != EOF) {
-        printf("%s %s %s %s %s\n", stt, name, mssv, major, birth);
-        i++;
+    while(fscanf(fp, "%s %s %s %s %s %s %s %s %s %s", stt[index], mssv[index], surname[index], middlename[index], givenname[index], sex[index], age[index], math[index], physics[index], chemistry[index]) != EOF) {
+        // printf("%s %s %s %s %s\n", stt, name, mssv, major, birth);
+        index++;
     }
 
-
-    for(int i = 0; i < 4; i++) {
-        printf("%s ", stt[i]);
-        printf("%s ", name[i]);
-        printf("%s ", mssv[i]);
-        printf("%s ", major[i]);
-        printf("%s ", birth[i]);
-        printf("\n");
-    }
-
-
-
-
-    // fclose(fp);
-
-    // char (array[2][10])[5];
-
-    // for (int i = 0; i < 5; i++) {
-    //     for (int j = 0; j < 2; j++) {
-    //         scanf(" ", &((array[j][10])[i]));
-    //     }
+    // printf("%d", index);
+    getStudents(stt, mssv, surname, middlename, givenname, sex, age, math, physics, chemistry, index);
+    // for(int i = 0; i < index; i++) {
+    //     printf("**\t%s\t", stt[i]);
+    //     printf("%s\t", mssv[i]);
+    //     printf("%s\t", surname[i]);
+    //     printf("%s\t", middlename[i]);
+    //     printf("%s\t", givenname[i]);
+    //     printf("%s\t", sex[i]);
+    //     printf("%s\t", age[i]);
+    //     printf("%s\t", math[i]);
+    //     printf("%s\t", physics[i]);
+    //     printf("%s\t", chemistry[i]);
+    //     printf("\n");
     // }
 
-    // for (int i = 0; i < 5; i++) {
-    //     printf("%d: \n", i);
-    //     for (int j = 0; j < 2; j++) {
-    //         printf("%d: ", j);
-    //         printf("%s\n", (array[j][10])[i]);
-    //     }
-    // }
 
-    // // fp = fopen("script.txt", "r"); 
-    // // while(fscanf(fp, "%s", stt) != EOF) {
-    // //     array[i] = stt;
 
-    // //     i++;
-    // // }
 
-    // // for (int j = 0; j < count; j++) {
-    // //     printf("%s", array[j]);
-    // // }
+    
     return 0;
 }
